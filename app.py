d4874e0f1029df5c1cc53954a1f131744c7b6994
Micheal Ogunfemi <mikebellami7@gmail.com>
2022-06-20 04:05:09 +0100

    update

--- app.py
+++ app.py
@@ -31,20 +31,35 @@ db = SQLAlchemy(app)
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String()))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.String(300), nullable=False)
-    website = db.Column(db.String(120))
-    talent = db.Column(db.Boolean,  default=False)
-    description = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    talent = db.Column(db.Boolean)
+    description = db.Column(db.String(500))
     shows = db.relationship('Show', backref='venue', lazy=True)
+
+
+
+    # id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String, nullable=False)
+    # city = db.Column(db.String(120), nullable=False)
+    # state = db.Column(db.String(120), nullable=False)
+    # address = db.Column(db.String(120), nullable=False)
+    # phone = db.Column(db.String(120), nullable=False)
+    # image_link = db.Column(db.String(500))
+    # facebook_link = db.Column(db.String(120))
+    # genres = db.Column(db.String(300), nullable=False)
+    # website = db.Column(db.String(120))
+    # talent = db.Column(db.Boolean,  default=False)
+    # description = db.Column(db.String(120))
+    # shows = db.relationship('Show', backref='venue', lazy=True)
 
     def __repr__(self):
         return f'<venue_id: {self.id}, name: {self.name}>'
@@ -71,18 +86,19 @@ class Artist(db.Model):
     def __repr__(self):
         return f'<artist: {self.id}, name: {self.name} >'
 
-    
-    class Show(db.Model):
-      __tablename__ = 'Show'
-      id = db.Column(db.Integer, primary_key=True)
-      venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-      artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-      start_time = db.Column(db.DateTime, nullable=False)
 
-      def __repr__(self):
-          return f'<show_id: {self.id}, venue_id: {self.venue_id}, artist_id: {self.artist_id}>'
-          
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Show(db.Model):
+  __tablename__ = 'Show'
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False)
+
+  def __repr__(self):
+      return f'<show_id: {self.id}, venue_id: {self.venue_id}, artist_id: {self.artist_id}>'
+      
+# TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
